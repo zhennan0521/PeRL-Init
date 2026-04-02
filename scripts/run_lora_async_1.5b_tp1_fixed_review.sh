b8f9232cf7ddac93ef6fc1af7551e83db09ac4d0
zhennan0521 <1641225799@qq.com>
2026-04-02 16:00:11 +0800

    [fixed] multinodes running exp + [update] Todo

--- scripts/run_lora_async_1.5b_tp1_fixed_review.sh
+++ scripts/run_lora_async_1.5b_tp1_fixed_review.sh
@@ -7,20 +7,16 @@ PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 export PYTHONPATH="${PROJECT_DIR}/modules/AReaL"
 export NCCL_ASYNC_ERROR_HANDLING=1
 export TORCH_NCCL_BLOCKING_WAIT=0
-
+export NCCL_DEBUG=WARN
 
 TIMESTAMP=$(date +%Y%m%d%H%M)
 LOG_DIR="${PROJECT_DIR}/outputs/logs"
 mkdir -p "$LOG_DIR"
 
-EXPERIMENT_NAME=lora-1.5b-dapo-async-4node-lr5e-5-new-machine
-
 python3 "${SCRIPT_DIR}/run_rl.py" \
-    --config "${SCRIPT_DIR}/lora_async_4node_1.5b_dapo.yaml" \
-    experiment_name=${EXPERIMENT_NAME} \
+    --config "${SCRIPT_DIR}/lora_async_1.5b_dapo.yaml" \
+    experiment_name=lora-1.5b-dapo-async-lr5e-5-tp1-fixed-review \
     trial_name=${TIMESTAMP} \
-    +scheduler.type=ray \
-    cluster.n_nodes=4 \
-    cluster.n_gpus_per_node=8 \
+    actor.optimizer.lr=5e-5 \
     +actor.archon.enable_compile=false \
-    "$@" 2>&1 | tee "${LOG_DIR}/${EXPERIMENT_NAME}_${TIMESTAMP}.log"
+    "$@" 2>&1 | tee "${LOG_DIR}/lora_async_1.5b_tp1_fixed_review_${TIMESTAMP}.log"
